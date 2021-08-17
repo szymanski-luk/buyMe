5d7399eecad5e55e5aa8e957073244c63e1efd05
Łukasz Szymański <61495775+szyman-9@users.noreply.github.com>
2021-08-18 00:29:54 +0200

    Initial commit

---
+++ database/migrations/2021_08_17_215823_create_adverts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 20);
+            $table->text('content');
+            $table->string('img', 80);
+            $table->integer('price');
+            $table->string('city');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
